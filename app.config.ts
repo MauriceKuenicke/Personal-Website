2ac2a707dfb584b8a792969d6227ec19be544ac4
Maurice Künicke <47433175+MauriceKuenicke@users.noreply.github.com>
2023-12-18 20:52:13 +0100

    Updates

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,15 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Maurice Künicke',
+    description: 'Welcome to my personal website! :)',
     image: {
       src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      alt: 'An image showcasing my website.',
       width: 400,
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -18,7 +18,7 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
@@ -26,13 +26,16 @@ export default defineAppConfig({
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: '',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'MauriceKuenicke'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
